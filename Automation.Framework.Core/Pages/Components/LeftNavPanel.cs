341c207a559c2bf8a775f4bfcc75b8f3ab2c4f9a
Andrey Vlasov <lion32211@yandex.ru>
2023-11-04 17:57:16 +0100

    Add LeftNavPanel component

---
+++ Automation.Framework.Core/Pages/Components/LeftNavPanel.cs
@@ -0,0 +1,51 @@
+﻿using Automation.Framework.Common.Abstractions;
+using Automation.Framework.Core.Pages.Locators;
+
+namespace Automation.Framework.Core.Pages.Components;
+
+public class LeftNavPanel
+{
+    private readonly IWebDriverWrapper browser;
+    private readonly LeftNavPanelLocators repo;
+    private readonly ILogging log;
+
+    public LeftNavPanel(IWebDriverWrapper browser, LeftNavPanelLocators repo, ILogging log)
+    {
+        this.browser = browser;
+        this.repo = repo;
+        this.log = log;
+    }
+
+    public void ExpandLeftPanelMenu(string menu)
+    {
+        ToggleLeftPanelMenu(menu, true);
+    }
+
+    public void CollapseLeftPanelMenu(string menu)
+    {
+        ToggleLeftPanelMenu(menu, false);
+    }
+
+    private void ToggleLeftPanelMenu(string menu, bool expand)
+    {
+        var isPanelOpened = browser.GetElementFromDOM(repo.LeftMenuContent(menu))
+            .GetAttribute("className")
+            .Contains("element-list collapse show");
+
+        if ((expand && !isPanelOpened) || (!expand && isPanelOpened))
+        {
+            browser.FindElement(repo.LeftMenuByName(menu)).Click();
+        }
+
+        log.Information($"Execute ToggleLeftPanelMenu method with {(expand ? "expand" : "collapse")}");
+    }
+
+    public void OpenSubMenu(string subMenuName)
+    {
+        var subMenu = browser.FindElement(repo.ElementsSubMenuByName(subMenuName), 20);
+        browser.ExecuteAsyncJSScriptForElement("arguments[0].scrollIntoView();", subMenu);
+        subMenu.Click();
+
+        log.Information($"Execute OpenSubMenu method for menu: {subMenuName}");
+    }
+}
