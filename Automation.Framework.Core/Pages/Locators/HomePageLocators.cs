d5a6503286d737e672e2bc227002c8d82efd9377
Andrey Vlasov <lion32211@yandex.ru>
2023-11-04 17:56:08 +0100

    Add HomePage POM

---
+++ Automation.Framework.Core/Pages/Locators/HomePageLocators.cs
@@ -0,0 +1,12 @@
+﻿namespace Automation.Framework.Core.Pages.Locators;
+
+public class HomePageLocators : BaseLocators
+{
+    public string StartNavigationPanel => "//div[@class = 'category-cards']";
+    public string StartMenuByName(string name) =>
+        $"{StartNavigationPanel}//h5[contains(text(),'{name}')]/../../..";
+
+    public string TextBoxFieldByName(string fieldName) =>
+        $"//label[contains(text(),'{fieldName}')]/../following-sibling::*/*";
+    public string ButtonByName(string buttonName) => $"//button[contains(text(),'{buttonName}')]";
+}
