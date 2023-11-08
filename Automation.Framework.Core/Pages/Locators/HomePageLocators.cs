20f97f3e715a2334b9f6af2ad2908b525ced3c65
Andrey Vlasov <lion32211@yandex.ru>
2023-11-08 22:51:44 +0100

    Refactoring in locators

--- Automation.Framework.Core/Pages/Locators/HomePageLocators.cs
+++ Automation.Framework.Core/Pages/Locators/HomePageLocators.cs
@@ -3,10 +3,12 @@
 public class HomePageLocators : BaseLocators
 {
     public string StartNavigationPanel => "//div[@class = 'category-cards']";
+
     public string StartMenuByName(string name) =>
         $"{StartNavigationPanel}//h5[contains(text(),'{name}')]/../../..";
 
     public string TextBoxFieldByName(string fieldName) =>
         $"//label[contains(text(),'{fieldName}')]/../following-sibling::*/*";
+
     public string ButtonByName(string buttonName) => $"//button[contains(text(),'{buttonName}')]";
 }
