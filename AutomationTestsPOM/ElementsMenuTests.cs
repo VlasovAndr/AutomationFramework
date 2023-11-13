a72d8051eb69298ae326b53cf0a6947b83ea504f
Andrey Vlasov <lion32211@yandex.ru>
2023-11-13 22:29:40 +0100

    Added TextboxEmptyField test

--- AutomationTestsPOM/ElementsMenuTests.cs
+++ AutomationTestsPOM/ElementsMenuTests.cs
@@ -22,7 +22,7 @@ public class ElementsMenuTests : TestBase
         elementsPage = container.GetRequiredService<ElementsPage>();
     }
 
-    public static string[] MenuName =
+    public static string[] MenuNames =
     {
         LeftSubMenu.TextBox,
         LeftSubMenu.CheckBox,
@@ -36,16 +36,19 @@ public class ElementsMenuTests : TestBase
     };
 
     [Parallelizable(ParallelScope.All)]
-    [TestCaseSource(nameof(MenuName))]
+    [TestCaseSource(nameof(MenuNames))]
+    [Category("Elements_LeftPanel")]
     public void LeftPanelNavigationTest(string name)
     {
         elementsPage.Open();
         elementsPage.LeftPanel.OpenSubMenu(name);
+
         var title = elementsPage.GetTitleOnMainPage();
         title.Should().Be(name);
     }
 
     [Test]
+    [Category("Elements_TextBox")]
     public void TextboxValidData()
     {
         var user = new DataGeneratorService().GenerateRandomUser();
@@ -57,12 +60,23 @@ public class ElementsMenuTests : TestBase
 
         var outputWindowState = elementsPage.isOutputWindowPresent();
         outputWindowState.Should().BeTrue();
-
         var outputWindowText = elementsPage.GetOutputWindowText();
         outputWindowText.Should().Contain($"Name:{user.FullName}");
         outputWindowText.Should().Contain($"Email:{user.Email}");
         outputWindowText.Should().Contain($"Current Address :{user.CurrentAddress}");
         outputWindowText.Should().Contain($"Permananet Address :{user.PermanentAddress}");
+    }
+
+    [Test]
+    [Category("Elements_TextBox")]
+    public void TextboxEmptyField()
+    {
+        elementsPage.Open();
+        elementsPage.LeftPanel.OpenSubMenu(LeftSubMenu.TextBox);
+        elementsPage.ClickSubmitButton();
+
+        var outputWindowText = elementsPage.GetOutputWindowText();
+        outputWindowText.Should().BeEmpty();
     }
 
 }
